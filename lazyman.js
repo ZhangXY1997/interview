e695b6559644509772f0b76ef522d3271afa52ce
zxy <53554363+ZhangXY1997@users.noreply.github.com>
2026-03-11 19:08:51 +0800

    Update lazyman.js

--- lazyman.js
+++ lazyman.js
@@ -31,3 +31,41 @@ class Man {
 
 const a = new Man();
 a.sleep(3000).say('hi').sleep(2000).say('kkk')
+
+function LazyMan(name) {
+  const man = {};
+  man.queue = [];
+
+  man.eat = function (food) {
+    const fn = function () {
+      console.log(`eat ${food}`);
+    };
+    man.queue.push(fn);
+    return man;
+  };
+
+  man.sleep = function (time) {
+    const fn = async function () {
+      await new Promise(resolve => {
+        setTimeout(resolve, time * 1000);
+      });
+    };
+    man.queue.push(fn);
+    return man;
+  };
+
+  const init = async function () {
+    console.log(name);
+
+    for (let i = 0; i < man.queue.length; i++) {
+      await man.queue[i]();
+    }
+  };
+
+  setTimeout(() => {
+    init();
+  }, 0);
+
+  return man;
+}
+LazyMan('张三').sleep(1).eat('苹果').sleep(2).eat('香蕉');
